665d972cf0f05d9c519ae22596debe5fd6c5c280
dovantai <dovantai@192.168.2.166>
2018-08-02 17:24:34 +0900

    add test suit

---
+++ Test Suites/O1_Login_page_test.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>O1_Login_page_test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-02T17:06:47</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e25e87ef-1d51-4a08-be20-223e42f17357</testSuiteGuid>
+   <testCaseLink>
+      <guid>96199e5f-8a67-4f0d-93e9-4ed5fb7d6d18</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O1_Login_Page/Verify that Element display correctly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>17d76bf5-63ed-4316-a490-bb9b72700c9d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O1_Login_Page/Verify that user CAN NOT login with invalid credentials</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8d2676b-94dd-4716-9f82-0fa499a22b52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O1_Login_Page/Verify that can navigate to forget password page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c8d9dca-3f56-4073-aadf-fcee367d1e8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O1_Login_Page/Verify that user can login successful</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/O20_ForgetPw_Page_Test.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>O20_ForgetPw_Page_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-02T17:10:22</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>53cd5a8d-67e0-4fbb-8f6a-b9510d552218</testSuiteGuid>
+   <testCaseLink>
+      <guid>f9985049-d658-45ef-b21f-3f4c835b8d9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O20_ForgetPw_Page/Verify that Elements display correctly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fb095667-e673-4d0a-9611-af6d9e4908b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O20_ForgetPw_Page/Verify that user CAN NOT reset with invalid credentials</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a64808fe-239d-49ac-a694-26b2d4abf1e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O20_ForgetPw_Page/Verify that user can send reset email successful</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6a44cfd-415b-418a-b2ff-1d1642de76d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/O20_ForgetPw_Page/Verify that user can back to login page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
